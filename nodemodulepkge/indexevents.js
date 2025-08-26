643aaa3ad9030e3a939df89de04740fcc0287731
Avinash BT <84569613+avinashbt@users.noreply.github.com>
2025-08-26 22:50:06 +0530

    events

---
+++ nodemodulepkge/indexevents.js
@@ -0,0 +1,22 @@
+// Event Modules
+// How to use it?
+
+// create, fire(emit), listen(on)
+
+const event = require("events");
+const eventEmitter = new event.EventEmitter();
+
+// Create
+eventEmitter.on("eventName", (data) => {
+  console.log("Event Fired: ", data);
+});
+
+// Create
+eventEmitter.on("eventName", (data) => {
+  console.log("Event Fired: ", data);
+});
+
+// Fire (Emit)
+eventEmitter.emit("eventName", "asdasd");
+
+ 
